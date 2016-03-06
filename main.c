ac609483fd5d307605b20070f98c3b99c6e11f2c
RhinoDevel <rhinodevel@gmx.net>
2016-03-06 14:38:28 +0100

    Added JSON test strings and tiny "automatic" tests.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 // MT, 2016feb29
 
-//#define NDEBUG
+//NDEBUG
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,10 +9,38 @@
 
 int main()
 {
-    bool const isValid = json_parse("[1e-3,\"Eat my \\\"shorts!\",2E+5,3E4,null,[0,false,123,[]],[[]],[[false,-8.342,false],77e7]]", false);
+    char /*const*/ * const testStr[] = {
+       "{}",
+       "[]",
+       "[{}]",
+       "{\"a\":null}",
+       "{\"a\":null,\"b\":null}",
+       "{\"a\":1234,\"b\":1234.5678}",
+       "{\"a\":{\"b\":null}}",
+       "{\"a\":{\"b\":null,\"c\":true}}",
+       "{\"a\":{\"a-b\":null,\"a-c\":true}}",
+       "{\"a\":[],\"b\":null}",
+       "{\"a\":{},\"b\":false}",
+       "{\"a\":{\"a1\":[[{}]],\"a2\":null},\"myArr\":[1e-3,\"Eat my \\\"shorts!\",2E+5,3E4,null,[0,false,123,[]],[[]],[[false,-8.342,false],77e7]]}",
+       "{\"myArr\":[],\"myObj\":{}}",
+       "[{\"myArr\":[],\"myObj\":{\"subArr\":[null,false,true,0.0,-42,\"beerbarreltree\"],\"subObj\":{}}}]"
+    };
+    int const testStrCnt = (int)(sizeof testStr/sizeof *testStr),
+        startIndex = testStrCnt-1;
+    int i = 0;
 
-    printf(isValid?"VALID":"INVALID");
-    printf("\n");
+    for(i = startIndex;i<testStrCnt;++i)
+    {
+        if(!json_parse(testStr[i], false))
+        {
+            printf("Test at index %d failed: \"%s\"!\n", i, testStr[i]);
+            break;
+        }
+    }
+    if(i==testStrCnt)
+    {
+        printf("All %d test(-s) ran succeeded.\n", testStrCnt-startIndex);
+    }
 
     return EXIT_SUCCESS;
 }
